23090dbb2c6cacf6c0c170f57202bea98b4e51e8
diegol1101 <ddiegoleal@gmail.com>
2023-09-07 21:23:10 -0500

    fix: :tada: tokens

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAplicacionServices();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddJwt(builder.Configuration);
 builder.Services.AddDbContext<DbAppContext>(options =>
 {
